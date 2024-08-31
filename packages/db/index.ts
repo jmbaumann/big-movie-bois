b81e53d769d8f4eee0087ec432dcaf021be1b403
Jeremy Baumann <jeremymbaumann@gmail.com>
2024-08-31 15:43:43 -0600

    add fantasy film schema

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,19 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 export * from "@prisma/client";
-export type {
-  LeagueYear,
-  Studio,
-  Movie,
-  TMDBMovie,
-  LeagueSettings,
-  DraftState,
-  DraftStateUpdate,
-  AwardShow,
-  AwardShowYear,
-  AwardShowCategory,
-  AwardShowGroup,
-} from "./types";
 
 const globalForPrisma = globalThis as { prisma?: PrismaClient };
 

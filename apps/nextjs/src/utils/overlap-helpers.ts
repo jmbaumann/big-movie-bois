b4367113ebdb322b4e13c97609f1a5e8f443c125
Jeremy Baumann <jeremymbaumann@gmail.com>
2024-09-12 11:34:35 -0600

    overlap-helper title check

--- apps/nextjs/src/utils/overlap-helpers.ts
+++ apps/nextjs/src/utils/overlap-helpers.ts
@@ -189,3 +189,9 @@ function restructure(movie: OverlapMovie) {
     writers,
   };
 }
+
+function getMatchingStart(str1: string, str2: string) {
+  let i = 0;
+  while (i < str1.length && i < str2.length && str1[i] === str2[i]) i++;
+  return str1.substring(0, i);
+}

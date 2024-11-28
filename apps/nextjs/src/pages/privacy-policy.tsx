3e3f12055542aa641e417f725828adfa38e4f357
Jeremy Baumann <jeremymbaumann@gmail.com>
2024-11-28 11:05:53 -0700

    tos & privacy policy

---
+++ apps/nextjs/src/pages/privacy-policy.tsx
@@ -0,0 +1,9 @@
+import Layout from "~/layouts/main/Layout";
+
+export default function Home() {
+  return (
+    <Layout showFooter>
+      <p>Privacy Policy</p>
+    </Layout>
+  );
+}
---
+++ apps/nextjs/src/pages/terms-of-service.tsx
@@ -0,0 +1,9 @@
+import Layout from "~/layouts/main/Layout";
+
+export default function Home() {
+  return (
+    <Layout showFooter>
+      <p>Terms of Service</p>
+    </Layout>
+  );
+}

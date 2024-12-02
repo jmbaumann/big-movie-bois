dbef5eca29f4bde51c9ec46a8292e66f4dcd7f49
Jeremy Baumann <jeremymbaumann@gmail.com>
2024-12-01 21:46:01 -0700

    public sessions form

--- apps/nextjs/src/features/admin/fantasy-film/PublicSessions.tsx
+++ apps/nextjs/src/features/admin/fantasy-film/PublicSessions.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { format } from "date-fns";
 import { Pencil, Trash, X } from "lucide-react";
@@ -8,29 +9,115 @@ import { DRAFT_TYPES, STUDIO_SLOT_TYPES } from "@repo/api/src/enums";
 import { LeagueSessionSettings, updateLeagueSessionInputObj } from "@repo/api/src/router/fantasy-film/zod";
 import { LeagueSession } from "@repo/db";
 
-import { api } from "~/utils/api";
+import { api, RouterOutputs } from "~/utils/api";
 import { useArray } from "~/utils/hooks/use-array";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "~/components/ui/accordion";
 import { Button } from "~/components/ui/button";
 import { Form } from "~/components/ui/form";
+import { useConfirm } from "~/components/ui/hooks/use-confirm";
 import { useToast } from "~/components/ui/hooks/use-toast";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "~/components/ui/sheet";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/table";
 import { DetailsSection } from "~/features/fantasy-film/forms/Session";
 import { ONE_DAY_IN_SECONDS } from "~/utils";
 
+type League = RouterOutputs["ffLeague"]["getById"];
+type Session = NonNullable<League>["sessions"][number];
+
 export default function PublicSessionsAdmin() {
   const { toast } = useToast();
+  const confirm = useConfirm();
+
+  const [selectedSession, setSelectedSession] = useState<Session>();
 
   const { data: publicLeague, refetch } = api.ffLeague.getById.useQuery(
     { id: "cm18qkt8o00056e9iscpz7ym8" },
     { staleTime: ONE_DAY_IN_SECONDS },
   );
-  const { isLoading: creating, mutate: createSession } = api.ffLeagueSession.create.useMutation();
-  const { isLoading: updating, mutate: updateSession } = api.ffLeagueSession.update.useMutation();
   const { mutate: deleteSession } = api.ffLeagueSession.delete.useMutation();
 
+  async function handleDelete(id: string) {
+    const ok = await confirm("Are you sure you want to delete this Session?");
+    if (ok)
+      deleteSession(
+        { id },
+        {
+          onSuccess: () => {
+            toast({
+              title: "Session Deleted",
+            });
+            refetch();
+          },
+          onError: (error) => {
+            toast({ title: error.message, variant: "destructive" });
+          },
+        },
+      );
+  }
+
+  return (
+    <div className="flex w-full flex-col">
+      <SessionFormSheet
+        className="bg-primary h-10 w-max rounded-3xl px-4 py-2 text-slate-50 hover:bg-teal-700/90"
+        refetch={refetch}
+      >
+        + New Session
+      </SessionFormSheet>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[40px]"></TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Start Date</TableHead>
+            <TableHead>End Date</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {publicLeague?.sessions?.map((session, i) => (
+            <TableRow key={i}>
+              <TableCell onClick={() => setSelectedSession(session)}>
+                <SessionFormSheet selectedSession={selectedSession} refetch={refetch}>
+                  <Pencil size={20} />
+                </SessionFormSheet>
+              </TableCell>
+              <TableCell>{session.name}</TableCell>
+              <TableCell>{format(session.startDate, "LLL dd, yyyy")}</TableCell>
+              <TableCell>{format(session.endDate, "LLL dd, yyyy")}</TableCell>
+              <TableCell>
+                <Button size="icon" variant="destructive" onClick={() => handleDelete(session.id)}>
+                  <Trash size={20} />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+function SessionFormSheet({
+  children,
+  className,
+  selectedSession,
+  refetch,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  selectedSession?: Session;
+  refetch: () => void;
+}) {
+  const { toast } = useToast();
+  const confirm = useConfirm();
+
+  const [open, setOpen] = useState(false);
+
+  const { isLoading: creating, mutate: createSession } = api.ffLeagueSession.create.useMutation();
+  const { isLoading: updating, mutate: updateSession } = api.ffLeagueSession.update.useMutation();
+
   const isLoading = creating || updating;
-  const sessionMembers = useArray<string>(publicLeague?.members.map((e) => e.userId) ?? []);
 
   const formSchema = updateLeagueSessionInputObj;
 
@@ -65,8 +152,19 @@ export default function PublicSessionsAdmin() {
     },
   });
 
+  useEffect(() => {
+    if (selectedSession) {
+      form.setValue("id", selectedSession.id);
+      form.setValue("name", selectedSession.name);
+      form.setValue("startDate", selectedSession.startDate);
+      form.setValue("endDate", selectedSession.endDate);
+      form.setValue("settings.teamStructure", selectedSession.settings.teamStructure);
+    }
+  }, [selectedSession]);
+
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
+    values.settings.draft.conduct = false;
     if (!values.id)
       createSession(values, {
         onSuccess: (data) => {
@@ -75,6 +173,7 @@ export default function PublicSessionsAdmin() {
           });
           form.reset();
           refetch();
+          setOpen(false);
         },
         onError: (error) => {
           toast({ title: error.message, variant: "destructive" });
@@ -88,6 +187,7 @@ export default function PublicSessionsAdmin() {
           });
           form.reset();
           refetch();
+          setOpen(false);
         },
         onError: (error) => {
           toast({ title: error.message, variant: "destructive" });
@@ -95,90 +195,27 @@ export default function PublicSessionsAdmin() {
       });
   }
 
-  function handleEdit(session: LeagueSession) {
-    form.setValue("id", session.id);
-    form.setValue("name", session.name);
-    form.setValue("startDate", session.startDate);
-    form.setValue("endDate", session.endDate);
-    form.setValue(
-      "settings.teamStructure",
-      JSON.parse(session.settings as string)!.teamStructure as LeagueSessionSettings["teamStructure"],
-    );
-  }
-
-  function handleDelete(id: string) {
-    deleteSession(
-      { id },
-      {
-        onSuccess: () => {
-          toast({
-            title: "Session Deleted",
-          });
-          refetch();
-        },
-        onError: (error) => {
-          toast({ title: error.message, variant: "destructive" });
-        },
-      },
-    );
-  }
-
   const onInvalid = (errors: any) => console.log({ errors });
 
   return (
-    <div className="flex w-full flex-col">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit, onInvalid)}>
-          <div className="float-right flex items-center gap-x-2">
-            <Button type="button" size="icon" variant="ghost" onClick={() => form.reset()}>
-              <X />
-            </Button>
-            <Button type="submit" onClick={() => onSubmit(form.getValues())} isLoading={isLoading}>
-              Save
-            </Button>
-          </div>
-
-          <Accordion className="w-5/6" type="single" collapsible>
-            <AccordionItem value="settings">
-              <AccordionTrigger>General</AccordionTrigger>
-              <AccordionContent className="mt-2 space-y-8 px-4">
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger className={className}>{children}</SheetTrigger>
+      <SheetContent className="w-1/2">
+        <SheetHeader>
+          <SheetTitle>{selectedSession ? "Edit" : "New"} Session</SheetTitle>
+          <SheetDescription>
+            <Form {...form}>
+              <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit, onInvalid)}>
                 <DetailsSection />
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        </form>
-      </Form>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[40px]"></TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Start Date</TableHead>
-            <TableHead>End Date</TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {publicLeague?.sessions?.map((session, i) => (
-            <TableRow key={i}>
-              <TableCell>
-                <Button className="hover:text-primary" size="icon" variant="ghost" onClick={() => handleEdit(session)}>
-                  <Pencil size={20} />
+                <Button className="mt-4" type="submit" onClick={() => onSubmit(form.getValues())} isLoading={isLoading}>
+                  Save
                 </Button>
-              </TableCell>
-              <TableCell>{session.name}</TableCell>
-              <TableCell>{format(session.startDate, "LLL dd, yyyy")}</TableCell>
-              <TableCell>{format(session.endDate, "LLL dd, yyyy")}</TableCell>
-              <TableCell>
-                <Button size="icon" variant="destructive" onClick={() => handleDelete(session.id)}>
-                  <Trash size={20} />
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+              </form>
+            </Form>
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
   );
 }

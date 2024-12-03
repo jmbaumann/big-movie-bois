dd4ae45a8543b454bab39f44cae54a10ca4940e9
Jeremy Baumann <jeremymbaumann@gmail.com>
2024-12-03 08:37:51 -0700

    use browser date for overlap answer

--- apps/nextjs/src/features/daily-games/overlap/OverlapPage.tsx
+++ apps/nextjs/src/features/daily-games/overlap/OverlapPage.tsx
@@ -73,7 +73,7 @@ export default function OverlapPage() {
 
   const { data: answer } = api.overlap.getAnswer.useQuery(
     {
-      date: archive ?? undefined,
+      date: archive ?? format(new Date(), "yyyy-MM-dd"),
     },
     { staleTime: ONE_DAY_IN_SECONDS },
   );

ee1cce54789a453270f4f3ac7823b11a077106fe
Jeremy Baumann <jeremymbaumann@gmail.com>
2024-12-10 15:32:18 -0700

    ResponsiveDialog cleanup

--- apps/nextjs/src/components/ResponsiveDialog.tsx
+++ apps/nextjs/src/components/ResponsiveDialog.tsx
@@ -11,62 +11,6 @@ import {
 } from "~/components/ui/dialog";
 import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "~/components/ui/sheet";
 
-// export default function ResponsiveDialog({
-//   trigger,
-//   footer,
-//   children,
-//   title,
-//   className,
-//   side,
-//   open,
-//   setOpen,
-//   forceMount,
-// }: {
-//   trigger?: React.ReactNode;
-//   footer?: React.ReactNode;
-//   children: React.ReactNode;
-//   title: string;
-//   className?: string;
-//   side?: "top" | "bottom" | "left" | "right" | null | undefined;
-//   open?: boolean;
-//   setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
-//   forceMount?: boolean;
-// }) {
-//   const breakpoint = useBreakpoint();
-
-//   if (!breakpoint.isMobile)
-//     return (
-//       <Dialog>
-//         <DialogTrigger className={className}>{trigger}</DialogTrigger>
-//         <DialogContent className="max-w-2/3 max-h-[90%] w-2/3 overflow-y-auto">
-//           <DialogHeader>
-//             <DialogTitle>{title}</DialogTitle>
-//             <DialogDescription className="text-white">{children}</DialogDescription>
-//           </DialogHeader>
-//           <Footer />
-//         </DialogContent>
-//       </Dialog>
-//     );
-
-//   return (
-//     <Sheet open={open} onOpenChange={setOpen}>
-//       <SheetTrigger className="px-2 py-2">{trigger}</SheetTrigger>
-//       <SheetContent
-//         side={side ?? "bottom"}
-//         className={cn("scrollbar-hidden max-h-[90%] bg-neutral-900 text-white", className)}
-//         aria-describedby={undefined}
-//       >
-//         <SheetHeader>
-//           <SheetTitle className="text-lg font-semibold text-white">{title}</SheetTitle>
-
-//           {children}
-//         </SheetHeader>
-//         <Footer />
-//       </SheetContent>
-//     </Sheet>
-//   );
-// }
-
 const ResponsiveDialog = ({
   children,
   open,
